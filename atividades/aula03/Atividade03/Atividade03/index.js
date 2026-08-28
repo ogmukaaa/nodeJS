fcbd613b5aa9fbee0cc0a3edd72081ca89befdf1
ogmukaaa <samuel.s.santos26@aluno.senai.br>
2026-08-28 16:19:06 -0300

    atividade 2 e 3 feitas!

--- atividades/aula03/Atividade03/Atividade03/index.js
+++ atividades/aula03/Atividade03/Atividade03/index.js
@@ -1,9 +1,15 @@
 const nomeInformado = process.argv[2];
 const turmaInformada = process.argv[3];
 
-console.log('Entrada recebida:', { nomeInformado, turmaInformada });
-console.error('PENDENTE: valide nome e turma antes de confirmar a inscrição.');
-process.exitCode = 1;
+if (!nomeInformado || !turmaInformada) {
+    console.error('PENDENTE: valide nome e turma antes de confirmar a inscrição.');
+    process.exitCode = 1;
+} else {
+    console.log('Entrada recebida:', { nomeInformado, turmaInformada });
+}
+
+
+
 
 
 // TODO: substitua as três linhas acima pela validação e pela confirmação pedidas.
