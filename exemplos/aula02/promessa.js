41032825d713e5b648435749edf381d776ac6c76
ogmukaaa <samuel.s.santos26@aluno.senai.br>
2026-09-01 16:58:33 -0300

    Aula sobre map,filter, async function, promise

---
+++ exemplos/aula02/promessa.js
@@ -0,0 +1,26 @@
+function consultarProduto(id) {
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=>{
+            if(id === 1) resolve ({id:1, nome: 'Mouse'});
+            else reject(new Error(`Produto ${id} não encontrado`));
+        },150);
+    })
+}
+// Chamada por Promise
+// consultarProduto(1)
+// .then((produto)=> console.log('Sucesso: ', produto))
+// .catch((erro)=>console.log('Falha: ', erro.message));
+
+
+// Chamada por Async Await
+async function executar(id) {
+    try {
+        const produto = await consultarProduto(id);
+        console.log('Sucesso: ', produto)
+    } catch (erro) {
+        console.error('Falha: ', erro.message);
+        process.exitCode = 1;
+    }
+};
+
+executar(1);
